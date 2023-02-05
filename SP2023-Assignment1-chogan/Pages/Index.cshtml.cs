09396ffb8c3f70403f3a5b1ce8f32f72f3d96a1d
Chloe Hogan <crhogan1@crimson.ua.edu>
2023-02-05 17:08:48 -0600

    done :)

---
+++ SP2023-Assignment1-chogan/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SP2023_Assognment1_chogan.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
